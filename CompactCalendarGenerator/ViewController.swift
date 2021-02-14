71c1b1f6d11fa283cbdf1b15da933949b8b1f1c7
Mulang Su <sumulang@gmail.com>
2021-02-14 09:09:21 +0800

    Initial commit

---
+++ CompactCalendarGenerator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CompactCalendarGenerator
+//
+//  Created by Mulang Su on 13/2/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
