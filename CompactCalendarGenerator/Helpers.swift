a26f06c15df2e0cba91152d188bc26fcb986dc37
Mulang Su <sumulang@gmail.com>
2021-02-18 22:20:37 +0800

    added monthsGroupedByDayOfWeekOfFirstDay

--- CompactCalendarGenerator/Helpers.swift
+++ CompactCalendarGenerator/Helpers.swift
@@ -19,3 +19,16 @@ func generateLabel(text: String, backgroundColor: UIColor) -> UILabel {
     return label
 }
 
+let monthDaysCountToColor = [
+    28: UIColor(named: "februaryColor")!,
+    29: UIColor(named: "februaryColor")!,
+    30: UIColor(named: "30daysColor")!,
+    31: UIColor(named: "31daysColor")!,
+]
+
+func monthsGroupedByDayOfWeekOfFirstDay(year: Int) -> [Int: [Month]] {
+    let allFirstDaysOfMonth = (1...12).map { DateInRegion(year: year, month: $0, day: 1, region: Region.ISO) }
+    return Dictionary(grouping: allFirstDaysOfMonth) { date in date.weekday }
+            .mapValues { $0.map { Month(rawValue: $0.month - 1)! } }
+}
+
