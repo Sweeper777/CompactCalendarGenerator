55b5e748e91621a373d2ad8a94a941db60134750
Mulang Su <sumulang@gmail.com>
2021-02-19 22:04:15 +0800

    added IBActions

--- CompactCalendarGenerator/ViewController.swift
+++ CompactCalendarGenerator/ViewController.swift
@@ -1,18 +1,37 @@
-//
-//  ViewController.swift
-//  CompactCalendarGenerator
-//
-//  Created by Mulang Su on 13/2/2021.
-//
-
+import SwiftDate
 import UIKit
 
 class ViewController: UIViewController {
+
+    var year = DateInRegion().year
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var shareButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+
+    @IBAction func plus1() {
+        year += 1
+        regenerateCalendar()
+    }
+
+    @IBAction func plus10() {
+        year += 10
+        regenerateCalendar()
+    }
+
+    @IBAction func minus1() {
+        year -= 1
+        regenerateCalendar()
+    }
+
+    @IBAction func minus10() {
+        year -= 10
+        regenerateCalendar()
+    }
 
 }
 
