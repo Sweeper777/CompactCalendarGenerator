1972d61f1039f89a21b1688fcffdd1bc77c5b240
Mulang Su <sumulang@gmail.com>
2021-02-23 22:05:49 +0800

    removed drawPrintFormatter

--- CompactCalendarGenerator/CalendarPrintPageRenderer.swift
+++ CompactCalendarGenerator/CalendarPrintPageRenderer.swift
@@ -13,8 +13,4 @@ class CalendarPrintPageRenderer: UIPrintPageRenderer {
         let scale = min(xScale, yScale)
         image.draw(in: CGRect(origin: printableRect.origin, size: CGSize(width: image.size.width * scale, height: image.size.height * scale)))
     }
-
-    override func drawPrintFormatter(_ printFormatter: UIPrintFormatter, forPageAt pageIndex: Int) {
-
-    }
 }
